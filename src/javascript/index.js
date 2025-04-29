ea3f7b1d95250019f69790f3eebd99a8bb78d5f6
NiklasKaulfers <niklas.kaulfers@zeiss.com>
2025-04-29 12:50:45 +0200

    updated styles

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,5 +1,9 @@
 import generateChart from "./chart-js-helper/chart-js-helper.js";
 
+
+
+loadCharts();
+
 async function loadCharts() {
     await generateChart({
         csvFilename: "../../data/calliopemini-data-2025-03-17T12-05-48-003Z.csv",
@@ -15,11 +19,29 @@ async function loadCharts() {
         borderColor: "rgb(0,100,255)",
         backgroundColor: "rgba(0,100,255,0.2)",
         title: "Messungen vom 17.03.2025",
-        animationIndex: 1
+        animationIndex: 2,
     })
 // generateChart({
 //     csvFilename: "/data/calliopemini-data-2025-03-19T09-49-18-231Z.csv"
 // })
 }
 
-loadCharts();
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
